3a3a6c4c54b1de42c62d84802a7ae0a25cbda7c6
Lucas <lmachado72@outlook.com>
2021-02-06 23:40:50 -0300

    Implementando estrutura de páginas/componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import Profile from './pages/Profile';
 import Repo from './pages/Repo';
 

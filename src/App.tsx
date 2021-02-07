438522e8e9a3a02f8534dea37ad7c10122a57526
Lucas <lmachado72@outlook.com>
2021-02-06 23:48:52 -0300

    Setando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Profile from './pages/Profile';
 import Repo from './pages/Repo';
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       {/* <Header /> */}
 
+      
       <Routes>
         <Route path="/" element={<Profile />} />
         <Route path="/:username" element={<Profile />} />
@@ -17,6 +19,8 @@ function App() {
       </Routes>
 
       {/* <Footer /> */}
+
+      <GlobalStyles />
     </BrowserRouter>
   );
 }

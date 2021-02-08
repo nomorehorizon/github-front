ac7088e4e39aede8c6a4646b4a932cdc95402b07
Lucas <lmachado72@outlook.com>
2021-02-08 18:12:51 -0300

    Repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { 
+  Container, 
+  Breadcrumb, 
+  RepoIcon, 
+  Stats, 
+  StarIcon, 
+  ForkIcon, 
+  LinkButton, 
+  GithubIcon 
+} from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={'/nomorehorizon'}>
+          nomorehorizon
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/nomorehorizon/github-front'}>
+          github-front
+        </Link>
+      </Breadcrumb>
+
+      <p>reproduzindo a interface do GitHub com React.js, incluindo navegação por rotas, responsividade e dark theme!</p>
+      
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>0</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/nomorehorizon/github-front'}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,5 +1,43 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { RiBookMarkLine, RiStarLine } from 'react-icons/ri';
+import { AiOutlineFork } from 'react-icons/ai';
+import { FaGithub } from 'react-icons/fa';
 
 export const Container = styled.div`
   
+`;
+
+export const Breadcrumb = styled.div`
+
+`;
+
+const iconCSS = css`
+    width: 16px;
+    height: 16px;
+    fill: var(--icon);
+    flex-shrink: 0;
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`
+    ${iconCSS};
+`;
+
+export const Stats = styled.div`
+
+`;
+
+export const StarIcon = styled(RiStarLine)`
+    ${iconCSS};
+`;
+
+export const ForkIcon = styled(AiOutlineFork)`
+    ${iconCSS};
+`;
+
+export const LinkButton = styled.a`
+
+`;
+
+export const GithubIcon = styled(FaGithub)`
+    ${iconCSS};
 `;

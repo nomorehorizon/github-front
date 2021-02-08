3e8b21fd355a18f8ef564caee9b1881296073390
Lucas <lmachado72@outlook.com>
2021-02-08 18:25:40 -0300

    CSS Repo page

--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -4,11 +4,45 @@ import { AiOutlineFork } from 'react-icons/ai';
 import { FaGithub } from 'react-icons/fa';
 
 export const Container = styled.div`
-  
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+
+    > p {
+        font-size: 16px;
+    }
 `;
 
 export const Breadcrumb = styled.div`
+    margin-bottom: 16px;
 
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    white-space: nowrap;
+    font-size: 18px;
+
+    > a {
+        color: var(--link);
+        text-decoration: none;
+
+        &:hover,
+        &:focus {
+            text-decoration: underline;
+        }
+
+        &.username {
+            margin-left: 8px;
+        }
+
+        &.reponame {
+            font-weight: 600;
+        }
+    }
+
+    > span {
+        padding: 0 5px;
+    }
 `;
 
 const iconCSS = css`
@@ -22,8 +56,20 @@ export const RepoIcon = styled(RiBookMarkLine)`
     ${iconCSS};
 `;
 
-export const Stats = styled.div`
+export const Stats = styled.ul`
+    margin-top: 16px;
+    display: flex;
+    align-items: center;
 
+    > li {
+        display: flex;
+        align-items: center;
+        margin-right: 9px;
+        > * {
+            margin-right: 7px;
+            color: var(--gray);
+        }
+    }
 `;
 
 export const StarIcon = styled(RiStarLine)`
@@ -35,7 +81,25 @@ export const ForkIcon = styled(AiOutlineFork)`
 `;
 
 export const LinkButton = styled.a`
+    text-decoration: none;
+    
+    margin-top: 24px;
+    background: var(--gray-dark);
+    padding: 12px 17px;
+    border-radius: 24px;
+    width: max-content;
+    
+    display: flex;
+    align-items: center;
 
+    > span {
+        color: var(--primary);
+    }
+    > svg {
+        fill: var(--primary);
+        margin-right: 10px;
+        margin-top: -3px;
+    }
 `;
 
 export const GithubIcon = styled(FaGithub)`
